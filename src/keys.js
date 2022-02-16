75a8c98a52ab8f4b78aca7d262de316a5df3595a
Xavier Villavicencio <jxavier2525@gmail.com>
2022-02-15 21:56:20 -0500

    first commit

---
+++ src/keys.js
@@ -0,0 +1,11 @@
+module.exports = {
+
+    database: {
+        connectionLimit: 10,
+        host: 'servidordblinks.database.windows.net',
+        user: 'xavierUNL',
+        password: 'Xavier46238148',
+        database: 'db_links'
+    }
+
+};
