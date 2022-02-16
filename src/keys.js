43e9228c8f6de9e795fa160cc67d5b2544d948f8
XavierlVill2525 <60786295+XavierlVill2525@users.noreply.github.com>
2022-02-16 14:12:32 -0500

    updaet

--- src/keys.js
+++ src/keys.js
@@ -2,9 +2,9 @@ module.exports = {
 
     database: {
         connectionLimit: 10,
-        host: 'servidordblinks.database.windows.net',
-        user: 'xavierUNL',
-        password: 'Xavier46238148',
+        host: 'servidormaria.mariadb.database.azure.com',
+        user: 'xavierUNL@servidormaria',
+        password: 'Unl46238148',
         database: 'db_links'
     }
 
